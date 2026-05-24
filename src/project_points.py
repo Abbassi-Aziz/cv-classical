9568ad3d4acba428b74b09334094cc073cabd463
Abbassi-Aziz <mohamedaziz.abbassi@insat.ucar.tn>
2026-05-24 22:47:39 +0100

    refactor: add commented-out alternative projection method

--- src/project_points.py
+++ src/project_points.py
@@ -9,5 +9,10 @@ point = (0.5 , 0.3 , 2.0)
 u = k[0][0]*point[0]/point[2] + k[0][2]
 v = k[1][1]*point[1]/point[2] + k[1][2]
 
+#otherwise:
+#projected = K @ point_as_vector   # matrix multiply
+#u = projected[0] / projected[2]   # divide by Z
+#v = projected[1] / projected[2]   # divide by Z
+
 print(u)
 print(v)
