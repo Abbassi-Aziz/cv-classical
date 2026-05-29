64a34061f67e6dcb84f73a2addb9c514ef363594
Abbassi-Aziz <mohamedaziz.abbassi@insat.ucar.tn>
2026-05-29 20:34:12 +0100

    feat: implemented cvlib functions

--- cvlib/features.py
+++ cvlib/features.py
@@ -1,7 +1,46 @@
 import numpy as np
+import cv2
+from cvlib.filters import conv_2d
 
 def harris(image, k=0.04, threshold = 0.01):
-    pass
+    Gx = np.array([[-1,0,1],
+               [-2,0,2],
+               [-1,0,1]], dtype=np.float32)
+    Gy = np.array([[-1,-2,-1],
+               [0,0,0],
+               [1,2,1]], dtype=np.float32)
+
+    Ix = conv_2d(image, Gx)
+    Iy = conv_2d(image,Gy)
+
+    Ix2 = Ix * Ix
+    Iy2 = Iy * Iy
+    Ixy = Ix * Iy
+
+    Ix2 = cv2.GaussianBlur(Ix2,(5,5),1.0)
+    Iy2 = cv2.GaussianBlur(Iy2,(5,5),1.0)
+    Ixy = cv2.GaussianBlur(Ixy,(5,5),1.0)
+
+    det_M = Ix2 * Iy2 - Ixy **2
+    trace_M = Ix2 + Iy2
+
+    R = det_M - k * (trace_M **2)
+    maxR = np.max(R)
+    thres = np.array(R> threshold*maxR)
+    R = R * thres
+    corners = np.argwhere(thres)
+    return corners, R
 
 def sift_detect_match(img1, img2,ratio=0.75):
-    pass
+    sift = cv2.SIFT_create()
+    kp1 , des1 = sift.detectAndCompute(img1, None)
+    kp2 , des2 = sift.detectAndCompute(img2, None)
+
+    bf = cv2.BFMatcher()
+    matches = bf.knnMatch(des1, des2, k=2)
+
+    good = []
+    for m,n in matches:
+        if m.distance / n.distance < ratio :
+            good.append(m)
+    return kp1, kp2, good
--- cvlib/geometry.py
+++ cvlib/geometry.py
@@ -1,7 +1,23 @@
 import numpy as np
+from cvlib.features import sift_detect_match
+import cv2
 
-def project_point(K,point_3d):
-    pass
+def project_point(K, point_3d):
+    projected = K @ point_3d
+    u = projected[0] / projected[2]
+    v = projected[1] / projected[2]
+    return u, v
 
 def compute_homography(img1,img2):
-    pass
+    kp1,kp2,good = sift_detect_match(img1,img2)
+    if len(good) > 4:
+        src_pts = np.float32([kp1[m.queryIdx].pt for m in good]).reshape(-1,1,2)
+        dst_pts = np.float32([kp2[m.trainIdx].pt for m in good]).reshape(-1,1,2)
+        H, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+    
+        warped_img = cv2.warpPerspective(img1, H, (img2.shape[1], img2.shape[0]))
+
+        return H, mask, warped_img
+    
+    else:
+        raise ValueError(f"Not enough good matches: {len(good)}, need at least 4")
